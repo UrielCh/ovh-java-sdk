86b208b12b284bc74cb75f96f4b0fe06ae58fe74
UrielCh <uriel.chemouni@gmail.com>
2017-04-18 08:51:37 -0700

    update exception handler

---
+++ ovh-java-sdk-core/src/main/java/net/minidev/ovh/core/OvhErrorMessage.java
@@ -0,0 +1,33 @@
+package net.minidev.ovh.core;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class OvhErrorMessage {
+	public static String INVALID_CREDENTIAL = "INVALID_CREDENTIAL";
+	public static String NOT_CREDENTIAL = "NOT_CREDENTIAL";
+	public static String QUERY_TIME_OUT = "QUERY_TIME_OUT";
+
+	/**
+	 * Ovh error code
+	 */
+	public String errorCode;
+	/**
+	 * http code + status line
+	 */
+	public String httpCode;
+	/*
+	 * error message
+	 */
+	public String message;
+
+	/**
+	 * this function will be deprecated and replace by an Enum;
+	 */
+	@JsonIgnore
+	public boolean isErrorCode(String... codes) {
+		for (String err : codes)
+			if (errorCode.equals(err))
+				return true;
+		return false;
+	}
+}
---
+++ ovh-java-sdk-core/src/main/java/net/minidev/ovh/core/OvhException.java
@@ -0,0 +1,29 @@
+package net.minidev.ovh.core;
+
+import java.io.IOException;
+
+@SuppressWarnings("serial")
+public class OvhException extends IOException {
+	private final OvhErrorMessage err;
+	private final String method;
+	private final String url;
+
+	public OvhException(String method, String url, OvhErrorMessage err) {
+		super(err.message);
+		this.method = method;
+		this.url = url;
+		this.err = err;
+	}
+
+	public OvhErrorMessage getOvhError() {
+		return err;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+}
